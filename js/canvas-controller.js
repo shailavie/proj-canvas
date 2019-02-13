b92ba454a5fa552d0a1eb1cb1dc0cf7ec8d2cdaf
Shai <shai.lavie@gmail.com>
2019-02-13 16:22:43 +0200

    expending controller

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -21,21 +21,23 @@ function init() {
 
 function onCanvasClick(ev){
     // debugger;
-    console.log(gIsFirstClick)
-    console.log(ev)
+    console.log('first click?',gIsFirstClick)
+    console.log('event',ev)
     if (gIsFirstClick) {
         gFirstClickPos = {
             x : ev.offsetX,
             y : ev.offsetY
         }
         console.log('gFirstClickPos',gFirstClickPos)
+        gIsFirstClick = !gIsFirstClick;
+        return;
     } else {
         var gCurrPos = {
             xEnd : ev.offsetX,
             yEnd : ev.offsetY
         }
     }
-    gCtx.rect(gFirstClickPos.x, gFirstClickPos.y, 50, 50);
+    gCtx.rect(gFirstClickPos.x, gFirstClickPos.y, gCurrPos.xEnd-gFirstClickPos.x, gCurrPos.yEnd-gFirstClickPos.y);
     gCtx.stroke()
     gIsFirstClick = !gIsFirstClick;
 }
