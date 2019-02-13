b4dd18753026e5fdbb29da3c1c63a37b86760dc8
Shai <shai.lavie@gmail.com>
2019-02-13 15:15:16 +0200

    todos

---
+++ js/canvas-service.js
@@ -0,0 +1,7 @@
+'use strict'
+
+//TO DO - service func to say if mouse click is first or second
+//first click (x,y)
+//seocond click rect measures
+
+
---
+++ js/canvas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//TO DO - detecting mouse click on canvas
+
+
+//TO DO - onClear
+//TO DO - onSave
+//TO DO - onSetShape
+//TO DO - onSetColorStroke
+//TO DO - onSetColorFill
+//TO DO - onSetWidth
+//TO DO - onDownload
+
