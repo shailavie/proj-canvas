90b31759d5f1a76536ce8ba4a784770191dd4fde
Shai Lavie <shail@wix.com>
2020-04-13 21:42:28 +0300

    draw line

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -1,77 +1,110 @@
-'use strict';
+"use strict";
 
-var gCoordStart;
+var gCoordStart = {};
 var gCanvas;
 var gCtx;
+var drawing = false;
 
 function init() {
-    document.querySelector('.stroke-width-label').innerHTML = document.querySelector('.stroke-width').value;
-    console.clear();
-    gCanvas = document.querySelector('#our-canvas');
-    gCtx = gCanvas.getContext('2d')
-    // gIsFirstClick = true;
-    console.log(gCtx)
+  globalState();
+  document.querySelector(
+    ".stroke-width-label"
+  ).innerHTML = document.querySelector(".stroke-width").value;
+  gCanvas = document.querySelector("#canvas");
+  gCtx = gCanvas.getContext("2d");
+  console.log(gCtx);
 }
 
-function onCanvasMouseDown(ev) {
-    saveCoords(ev.offsetX, ev.offsetY)
+function onCanvasMouseDown({ offsetX, offsetY }) {
+  drawing = true;
+  saveCoords({ x: offsetX, y: offsetY });
+  drawPen(offsetX, offsetY);
 }
 
 function onCanvasMouseUp(ev) {
-    drawShape(ev.offsetX, ev.offsetY);
+  drawing = false;
 }
 
-function saveCoords(x, y) {
-    gCoordStart = {
-        x: x,
-        y: y
-    }
+function onCanvasMouseMove({ offsetX, offsetY }) {
+  if (drawing) {
+    drawPen(offsetX, offsetY);
+  }
 }
 
-
-
-function drawShape(x, y) {
-    var coordEnd = {
-        x: x,
-        y: y
-    }
-    gCtx.beginPath()
-    gCtx.strokeStyle = getState('strokeColor')
-    gCtx.lineWidth = getState('strokeWidth')
-    gCtx.fillStyle = getState('fillColor')
-    var shape = getState('shape');
-    switch (shape) {
-        case 'rectangle':
-            gCtx.rect(gCoordStart.x, gCoordStart.y, coordEnd.x - gCoordStart.x, coordEnd.y - gCoordStart.y);
-            gCtx.stroke()
-            gCtx.fill();
-            break;
-        case 'oval':
-            var radius = Math.sqrt(Math.abs(gCoordStart.x - coordEnd.x) ** 2 + Math.abs(gCoordStart.y - coordEnd.y) ** 2)
-            gCtx.arc(gCoordStart.x, gCoordStart.y, radius, 0, 2 * Math.PI);
-            gCtx.stroke()
-            gCtx.fill();
-            break;
-    }
-    gCtx.closePath()
+function saveCoords({ x, y }) {
+  gCoordStart = { x, y };
 }
 
+const drawPen = (x, y) => {
+  gCtx.beginPath();
+  gCtx.strokeStyle = getState("strokeColor");
+  gCtx.lineWidth = getState("strokeWidth");
+  gCtx.moveTo(gCoordStart.x, gCoordStart.y);
+  gCtx.lineTo(x, y);
+  gCoordStart = { x, y };
+  gCtx.stroke();
+  gCtx.closePath();
+};
+
+const clearCanvas = () => {
+  gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+}
 
 
 
 function onStrokeColorChange(elInput) {
-    updateState('strokeColor', elInput.value);
+  updateState("strokeColor", elInput.value);
 }
 
 function onFillColorChange(elInput) {
-    updateState('fillColor', elInput.value);
+  updateState("fillColor", elInput.value);
 }
 
 function onStrokeWidthChange(elRange) {
-    updateState('strokeWidth', elRange.value);
-    document.querySelector('.stroke-width-label').innerHTML = (elRange.value);
+  updateState("strokeWidth", elRange.value);
+  document.querySelector(".stroke-width-label").innerHTML = elRange.value;
 }
 
 function onShapeChange(elInput) {
-    updateState('shape', elInput.value);
+  updateState("shape", elInput.value);
+}
+
+function drawShape(x, y) {
+  console.log(gCoordStart);
+  var coordEnd = {
+    x: x,
+    y: y,
+  };
+  gCtx.beginPath();
+  gCtx.strokeStyle = getState("strokeColor");
+  gCtx.lineWidth = getState("strokeWidth");
+  gCtx.fillStyle = getState("fillColor");
+  var shape = getState("shape");
+  switch (shape) {
+    case "pen":
+      gCtx.lineTo(coordEnd.x, coordEnd.y);
+      gCtx.stroke();
+      gCtx.fill();
+      break;
+    case "rectangle":
+      gCtx.rect(
+        gCoordStart.x,
+        gCoordStart.y,
+        coordEnd.x - gCoordStart.x,
+        coordEnd.y - gCoordStart.y
+      );
+      gCtx.stroke();
+      gCtx.fill();
+      break;
+    case "oval":
+      var radius = Math.sqrt(
+        Math.abs(gCoordStart.x - coordEnd.x) ** 2 +
+          Math.abs(gCoordStart.y - coordEnd.y) ** 2
+      );
+      gCtx.arc(gCoordStart.x, gCoordStart.y, radius, 0, 2 * Math.PI);
+      gCtx.stroke();
+      gCtx.fill();
+      break;
+  }
+  gCtx.closePath();
 }
--- js/canvas-service.js
+++ js/canvas-service.js
@@ -1,36 +1,65 @@
-'use strict'
+"use strict";
+
+const globalState = () => {
+  const state = {
+    strokeColor: "#000000",
+    fillColor: "#ffffff",
+    strokeWidth: "4",
+    shape: "pen",
+  };
+  const getState = (prop) => STATE[prop];
+  const updateState = (prop, val) => (state[prop] = val);
+  const saveCanvas = (canvas) => {
+    saveToStorage("canvas", canvas.toDataUrl());
+  };
+  const loadCanvas = () => {
+    const loadedCanvas = loadFromStorage("canvas");
+    renderCanvas(loadedCanvas);
+  };
+  const downloadCanvas = (elLink) => {
+    elLink.href = gCanvas.toDataURL();
+    elLink.download = "my-img.jpg";
+  };
+  const publicApi = {
+    getState,
+    updateState,
+    saveCanvas,
+    loadCanvas,
+    downloadCanvas,
+  };
+  console.log('this is happening');
+  
+  return publicApi;
+};
 
 
 
 //TO DO - service func to say if mouse click is first or second
 //first click (x,y)
 //seocond click rect measures
-var gState = {
-    strokeColor: '#000000',
-    fillColor: '#ffffff',
-    strokeWidth: '16',
-    shape: 'rectangle'
-}
+const STATE = {
+  strokeColor: "#000000",
+  fillColor: "#ffffff",
+  strokeWidth: "4",
+  shape: "pen",
+};
 
 function saveCanvas(elCanvas) {
-    saveToStorage('canvas', elCanvas.toDataUrl())
+  saveToStorage("canvas", elCanvas.toDataUrl());
 }
 
 function loadCanvas() {
-    var canvas = loadFromStorage('canvas');
-    renderCanvas(canvas);
+  var canvas = loadFromStorage("canvas");
+  renderCanvas(canvas);
 }
 
 function updateState(prop, val) {
-    gState[prop] = val;
+  STATE[prop] = val;
 }
 
-function getState(prop) {
-    return gState[prop];
-}
-
+const getState = (prop) => STATE[prop];
 
 function downloadCanvas(elLink) {
-    elLink.href = gCanvas.toDataURL()
-    elLink.download = 'my-img.jpg'
+  elLink.href = gCanvas.toDataURL();
+  elLink.download = "my-img.jpg";
 }
